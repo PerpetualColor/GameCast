3187aff519af05d7a986d056baf026525d8c9fc5
Jashan Pabla <jashanpabla@Jashans-MacBook-Pro.local>
2020-01-24 13:30:42 -0800

    started frontend scoreboard. Over

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,18 +7,22 @@ import { GameDisplayComponent } from './game-display/game-display.component';
 import { TitlebarComponent } from './titlebar/titlebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule} from '@angular/material';
 import { MatButtonModule } from '@angular/material';
 import { HomeComponent } from './home/home.component';
+import { GameScoreBoardComponent } from './game-score-board/game-score-board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameDisplayComponent,
     TitlebarComponent,
-    HomeComponent
+    HomeComponent,
+    GameScoreBoardComponent
   ],
   imports: [
     BrowserModule,
+    MatCardModule,
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
