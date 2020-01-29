da0cf14cf4fc694f8ddb55bb92a49267d7c01076
FyreFly00 <rwalkerwhf@gmail.com>
2020-01-29 12:22:53 -0800

    Partially complete GameController. Needs a few more functions. (This is just a test commit)

--- backend/src/main/java/xyz/advtopics/controllers/GameController.java
+++ backend/src/main/java/xyz/advtopics/controllers/GameController.java
@@ -1,19 +1,91 @@
 package xyz.advtopics.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import xyz.advtopics.objects.Event;
+import xyz.advtopics.objects.Team;
 import xyz.advtopics.objects.DTOs.GameDTO;
+import xyz.advtopics.services.GameService;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 @RestController
 public class GameController {
+    @Autowired 
+    private GameService gameService;
     
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    //Doesn't this need some form of name?
     @PostMapping("/createGame")
-    public ResponseEntity<String> createGame(@RequestBody GameDTO gameInfo) {
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Created");
+    public ResponseEntity<String> createGame() {
+        gameService.createGame();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Created");               
     }
+
+    @PostMapping("/addTeamToGame")
+    public ResponseEntity<String> addTeamToGame(long gameID, long teamID, long groupID){
+        Session session = sessionFactory.openSession();
+        
+        gameService.addTeamToGame(gameID, teamID);
+
+        session.close();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Created"); 
+    }
+
+    @PostMapping("/addEventToGame") 
+    public ResponseEntity<String> addEventToGame(long gameID, long eventID, long groupID){
+        Session session = sessionFactory.openSession();
+        
+        gameService.addTeamToGame(gameID, eventID);
+
+        session.close();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Created"); 
+    }
+
+    @PostMapping("/getTeams")
+
+    public ResponseEntity<List<Team>> getTeams(long gameID, long groupID){
+        Session session = sessionFactory.openSession();
+        
+        List<Team> teams = gameService.getTeams(gameID);
+
+        session.close();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(teams); 
+    }
+
+    @PostMapping("/getEvents")
+    public ResponseEntity<List<Event>> getEvents(long gameID, long groupID){
+        Session session = sessionFactory.openSession();
+        
+        List<Event> events = gameService.getEvents(gameID);
+
+        session.close();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(events); 
+    }
+
+    @PostMapping("/setEvents")
+
+    // Es tan roto: Necesito una manera de encontrar los eventos que transmite el frontend
+    public ResponseEntity<String> addEventToGame(long gameID,  long groupID){
+        Session session = sessionFactory.openSession();
+        
+        gameService.addEvents(gameID, );
+
+        session.close();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Created"); 
+    }
+
+
+
 
 }
---
+++ backend/src/main/java/xyz/advtopics/services/GameService.java
@@ -0,0 +1,97 @@
+package xyz.advtopics.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import xyz.advtopics.objects.Event;
+import xyz.advtopics.objects.Game;
+import xyz.advtopics.objects.Team;
+import xyz.advtopics.objects.DTOs.GameDTO;
+import xyz.advtopics.objects.DTOs.TeamDTO;
+
+@Service
+public class GameService {
+        
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void createGame() {
+        Session session = sessionFactory.openSession();
+        Game game = new Game();
+
+        List<Team> teams = new ArrayList<Team>();
+        game.setTeams(teams);
+        session.beginTransaction();
+        session.persist(game);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void addTeamToGame(long gameID, long teamID) {
+        Session session = sessionFactory.openSession();
+
+        Game game = session.get(Game.class, gameID);
+        Team team = session.get(Team.class, teamID);
+        game.addTeamToGame(team);
+
+        session.beginTransaction();
+        session.persist(game);
+        session.getTransaction().commit();
+        session.close();
+
+    }
+
+    public void addEvent(long gameID, long eventID) {
+        Session session = sessionFactory.openSession();
+        Game game = session.get(Game.class, gameID);
+        Event event = session.get(Event.class, eventID);
+        game.addEvents(event);
+
+        session.beginTransaction();
+        session.persist(game);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public List<Team> getTeams(long gameID) {
+        Session session = sessionFactory.openSession();
+        Game game = session.get(Game.class, gameID);
+        List<Team> teams = game.getTeams();
+        session.beginTransaction();
+        session.persist(game);
+        session.getTransaction().commit();
+        session.close();
+        
+        return teams;
+    }
+
+    public List<Event> getEvents(long gameID) {
+        Session session = sessionFactory.openSession();
+        Game game = session.get(Game.class, gameID);
+        List<Event> events = game.getEvents();
+        session.beginTransaction();
+        session.persist(game);
+        session.getTransaction().commit();
+        session.close();
+        
+        return events;
+    }
+
+
+    public void setEvents(long gameID, List<Event> events) {
+        Session session = sessionFactory.openSession();
+        Game game = session.get(Game.class, gameID);
+        game.setEvents(events);
+        session.beginTransaction();
+        session.persist(game);
+        session.getTransaction().commit();
+        session.close();
+    }
+}
