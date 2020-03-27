22684a69f40daef941a6df8b5dc928d84acd895e
Shayan Daijavad <shayan@Shayans-MacBook-Pro.local>
2020-03-27 11:21:52 -0700

    player stats roster stuff

--- frontend/src/app/roster/roster.component.ts
+++ frontend/src/app/roster/roster.component.ts
@@ -5,6 +5,7 @@ import { GameStatusService } from '../game-status.service';
 import { Player } from '../model-objects/player';
 import { RosterEditorComponent } from './roster-editor/roster-editor.component';
 import { MatDialog } from '@angular/material/dialog';
+import { PlayerStats } from '../model-objects/playerstats';
 
 @Component({
   selector: 'app-roster',
@@ -14,6 +15,17 @@ import { MatDialog } from '@angular/material/dialog';
 export class RosterComponent implements OnInit {
   guest: Team;
   home: Team;
+  displayedColumns: string[] = ['scores', 'fouls', 'rebounds'];
+  dataSource: PlayerStats[] = [
+    {scores: [7], fouls: 7, rebounds: 7},
+    {scores: [8], fouls: 8, rebounds: 8},
+    {scores: [9], fouls: 9, rebounds: 9},
+    {scores: [10], fouls: 2, rebounds: 10},
+    {scores: [34], fouls: 5, rebounds: 7},
+    {scores: [12], fouls: 11, rebounds: 3},
+    {scores: [13], fouls: 826, rebounds: 2},
+    {scores: [14], fouls: 9, rebounds: 24}
+  ];
 
   homeRoster: Player[];
 
@@ -22,9 +34,11 @@ export class RosterComponent implements OnInit {
   ngOnInit() {
     this.home = this.gameStatusService.game.teams[0];
     this.guest = this.gameStatusService.game.teams[1];
-
     
+
+   
   }
+  
 
   openEditorDialog(team: Team) {
     let dialogRef = this.editorDialog.open(RosterEditorComponent, {
