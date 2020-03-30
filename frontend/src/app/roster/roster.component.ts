56d909da6ad69b79c5ac12260176e2728f395003
Shayan Daijavad <shayan@Shayans-MacBook-Pro.local>
2020-03-30 16:00:30 -0700

    Roster is now a functioning table woohoo

--- frontend/src/app/roster/roster.component.ts
+++ frontend/src/app/roster/roster.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Team } from '../model-objects/team';
 import { BackendService } from '../backend.service';
 import { GameStatusService } from '../game-status.service';
@@ -15,30 +15,56 @@ import { PlayerStats } from '../model-objects/playerstats';
 export class RosterComponent implements OnInit {
   guest: Team;
   home: Team;
-  displayedColumns: string[] = ['scores', 'fouls', 'rebounds'];
-  dataSource: PlayerStats[] = [
-    {scores: [7], fouls: 7, rebounds: 7},
-    {scores: [8], fouls: 8, rebounds: 8},
-    {scores: [9], fouls: 9, rebounds: 9},
-    {scores: [10], fouls: 2, rebounds: 10},
-    {scores: [34], fouls: 5, rebounds: 7},
-    {scores: [12], fouls: 11, rebounds: 3},
-    {scores: [13], fouls: 826, rebounds: 2},
-    {scores: [14], fouls: 9, rebounds: 24}
+  displayedColumns: string[] = ['number', 'name', 'scores', 'fouls', 'rebounds'];
+  homeDataSource: Player[] = [
+    {id: -1, name: "James Xu", number: 1, stats: {scores: [7], fouls: 7, rebounds: 7}},
+    {id: -1, name: "James Xu", number: 1, stats: {scores: [8], fouls: 8, rebounds: 8}},
+    {id: -1, name: "James Xu", number: 1, stats: {scores: [9], fouls: 9, rebounds: 9}},
+    {id: -1, name: "James Xu", number: 1, stats: {scores: [10], fouls: 2, rebounds: 10}},
+    {id: -1, name: "James Xu", number: 1, stats: {scores: [34], fouls: 5, rebounds: 7}},
+    {id: -1, name: "James Xu", number: 1, stats: {scores: [12], fouls: 11, rebounds: 3}},
+    {id: -1, name: "James Xu", number: 1, stats: {scores: [13], fouls: 826, rebounds: 2}},
+    {id: -1, name: "James Xu", number: 1, stats: {scores: [14], fouls: 9, rebounds: 24}}
   ];
+
+  guestDataSource: Player[];
 
   homeRoster: Player[];
 
-  constructor(private backendService: BackendService, private gameStatusService: GameStatusService, public editorDialog: MatDialog) { }
+  constructor(private backendService: BackendService, 
+    private gameStatusService: GameStatusService, 
+    public editorDialog: MatDialog,
+    private changeDetectorRef: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.home = this.gameStatusService.game.teams[0];
     this.guest = this.gameStatusService.game.teams[1];
     
+    this.homeDataSource = this.gameStatusService.game.teams[0].players;
+    this.guestDataSource = this.gameStatusService.game.teams[1].players;
 
-   
+    for (let player of this.homeDataSource) {
+      if (!player.stats) {
+        player.stats = {scores: [], fouls: 0, rebounds: 0};
+      }
+    }
+
+    for (let player of this.guestDataSource) {
+      if (!player.stats) {
+        player.stats = {scores: [], fouls: 0, rebounds: 0};
+      }
+    }
+
+    this.gameStatusService.events$.subscribe({
+      next: result => {
+        this.changeDetectorRef.detectChanges();
+      }
+    })
   }
   
+  sumArray(numbers: number[]): number {
+    return numbers.reduce((a,b)=>a+b, 0);
+  }
 
   openEditorDialog(team: Team) {
     let dialogRef = this.editorDialog.open(RosterEditorComponent, {
@@ -49,6 +75,8 @@ export class RosterComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       this.gameStatusService.updateTeam();
+      this.ngOnInit();
+      this.changeDetectorRef.detectChanges();
     });
   }
 
